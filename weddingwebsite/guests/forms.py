2775ffccbd95b03f9bd292b694002efaa632929b
igbt6 <lukasz.uszko@gmail.com>
2018-07-24 03:16:40 +0200

    update

--- weddingwebsite/guests/forms.py
+++ weddingwebsite/guests/forms.py
@@ -1,10 +1,22 @@
 from django import forms
 
 
+
+PRESENCE_CONFIRMATION = (
+    (0, 'No I will not attend you wedding party :('), 
+    (1, 'Yes, I am going to celebrate your wedding with you :)')
+)
+
+HOTEL_CHOICES = (
+    (0, 'Not interested'), 
+    (1, 'Will stay in hotel')
+)
+
 class RsvpForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    confirm_or_not = forms.RadioSelect(choices=("Confirm", "Reject"))
-    send_email_with_info = forms.BooleanField()
-    email = forms.EmailField()
-    comments = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(label='Your name:', max_length=50)
+    presence_confirmation = forms.ChoiceField(label='Are you going to attend our wedding party?', choices=PRESENCE_CONFIRMATION, widget=forms.RadioSelect)
+    hotel_needed = forms.ChoiceField(label='Do you want to stay in hotel?', choices=HOTEL_CHOICES)
+    send_email_with_info = forms.BooleanField(label='Do you want to get an email with any update?')
+    email = forms.EmailField(label='Your email:')
+    comments = forms.CharField(label='Put you comments, questions below:', widget=forms.Textarea)
 
--- weddingwebsite/weddingwebsite/settings.py
+++ weddingwebsite/weddingwebsite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wedding',
-    'guests'
+    'guests',
+    'bootstrapform'
 ]
 
 MIDDLEWARE = [
