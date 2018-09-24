c9e97bb7cb310a0d26cc7c33fadbbe0b9352bd15
igbt6 <lukasz.uszko@gmail.com>
2018-09-24 20:11:21 +0200

    wedding utils config template file added

--- weddingutils/twilio_bot.py
+++ weddingutils/twilio_bot.py
@@ -17,7 +17,7 @@ class Twilio:
     def send_sms_msg(self, receiver_number, msg):
         message = self.client.messages.create(
                         body=msg,
-                        from_=self.your_twilio_num,
+                        from_= self.your_twilio_num,
                         to=receiver_number
                     )
         print(message.sid)
@@ -27,9 +27,10 @@ def send_sms_to_people_without_confirmation(guests_dict, twilio_client):
     if guests_dict is None:
         raise Exception('guests_dict is empty!')    
     # extract people without confirmation before the deadline date: 20.09.2018
-    msg_0 = 'Cześć, tutaj robot weselny Justyny i Łukasza. Wygląda na to że zbliża się termin potwierdzenia udziału w ich weselu. \
-             Proszę daj telefonicznie znać młodym jaki jest twój status. Jeśli potrzebujesz więcej czasu na zastanowienie, \
-             też ich o tym poinformuj, napewno się zgodzą :)  Justyna: 515323976 , Łukasz: 506305438. Pozdrawiam, Miłego dnia!'
+    msg_0 = 'Cześć, tutaj robot weselny Justyny i Łukasza. Wygląda na to że mija termin potwierdzenia udziału w ich weselu. \
+             Potwierdź proszę telefonicznie swoją obecność.\n Justyna: 515323976 , Łukasz: 506305438. Pozdrawiam, Miłego dnia!'
+    msg_1 = 'Cześć, tutaj Justyna i Łukasz. Wygląda na to że powoli mija termin potwierdzenia/odmówienia udziału w naszym weselu-27.10.2018\
+             Proszę, jeśli już wiesz, potwierdź/odmów telefonicznie swoją obecność.\n Justyna: 515323976, Łukasz: 506305438. Pozdrawiamy, Miłego wieczoru!'
     not_confirmed_list = []
     for i, _ in enumerate(guests_dict['name']):
         if guests_dict['invited'][i] == 'T' and guests_dict['confirmed'][i] == '-':
@@ -40,9 +41,10 @@ def send_sms_to_people_without_confirmation(guests_dict, twilio_client):
         #print("{} {} - {}".format(guest['name'], guest['surname'], guest['phone']))
         if guest['phone'] is not '-':
             print("sending sms to: {} {} - {}...".format(guest['name'], guest['surname'], guest['phone']))
-            #twilio_client.send_sms_msg(guest['phone'], msg_0)
+            twilio_client.send_sms_msg(guest['phone'], msg_1)
     ## test
-    #twilio_client.send_sms_msg('515 323 976', msg_0)  
+    twilio_client.send_sms_msg('515 323 976', msg_1)
+    twilio_client.send_sms_msg('506 305 438', msg_1)
 
 # main
 if __name__ == "__main__":
