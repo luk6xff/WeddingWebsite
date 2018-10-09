071b573ae45e44c3061e801da02d2221293dcfb7
igbt6 <lukasz.uszko@gmail.com>
2018-10-09 23:11:33 +0200

    alco_tags

--- weddingutils/twilio_bot.py
+++ weddingutils/twilio_bot.py
@@ -41,11 +41,11 @@ def send_sms_to_people_without_confirmation(guests_dict, twilio_client):
         #print("{} {} - {}".format(guest['name'], guest['surname'], guest['phone']))
         if guest['phone'] is not '-':
             print("sending sms to: {} {} - {}...".format(guest['name'], guest['surname'], guest['phone']))
-            twilio_client.send_sms_msg(guest['phone'], msg_1)
+            #twilio_client.send_sms_msg(guest['phone'], msg_1)
     ## test
-    twilio_client.send_sms_msg('515 323 976', msg_1)
-    twilio_client.send_sms_msg('506 305 438', msg_1)
-
+    #twilio_client.send_sms_msg('515 323 976', msg_1)
+    #twilio_client.send_sms_msg('506 305 438', msg_1)
+    twilio_client.send_sms_msg('+48794195468', "Hej jestem tylko botem weselnym, wiadomości pisz do Państwa młodych: Justyna: 515323976, Łukasz: 506305438. Pozdrawiam pięknie")
 # main
 if __name__ == "__main__":
     config = configparser.ConfigParser()
